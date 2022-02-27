44ee7a3537db6660664e2f487d2cf0d6067314fb
iwant2b1234 <iwant2b1234@gmail.com>
2022-02-27 17:15:29 +0800

    first commit

---
+++ weather-box/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted(){
+    console.log("jjjjjjj");
+    axios('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-B3945A19-3B00-4ED9-B55C-71D1A428BD78')
+    .then(function (response) {
+    console.log(response.data);
+  });
+  }
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
